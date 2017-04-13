d4f2bf311db4bf28e2a4f75719d2ce2f3fe5d08c
Tobias Derksen <t.derksen@gmx.de>
2017-04-13 20:22:58 +0200

    Added tailor project

---
+++ tailor/index.js
@@ -0,0 +1,22 @@
+// SOURCE: https://github.com/zalando/tailor/blob/master/examples/basic-css-and-js/index.js
+ 
+'use strict';
+
+const http = require('http');
+
+const Tailor = require('node-tailor');
+
+const tailor = new Tailor({
+    templatesPath: __dirname + '/templates'
+});
+
+// Root Server
+http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') {
+        res.writeHead(200, { 'Content-Type': 'image/x-icon' } );
+        return res.end('');
+    }
+    tailor.requestHandler(req, res);
+}).listen(3000, function () {
+    console.log('Tailor server listening on port 3000');
+});
